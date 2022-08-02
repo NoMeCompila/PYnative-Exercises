14411b3dbbf45da766c0c2d430c4d92041fdc24d
Fernando Caballero <fernando.caballero@globant.com>
2022-08-02 14:03:39 -0300

    Json Exercise 3

---
+++ Python Json Exercises/Exercise-3.py
@@ -0,0 +1,44 @@
+import json
+
+sampleJson = {"key1": "value1", "key2": "value2"}
+
+json_data = json.dumps(sampleJson,indent=2, separators=(",", " = "))
+
+print(json_data)
+
+
+
+reqres_data = {
+    "page": 2,
+    "per_page": 6,
+    "total": 12,
+    "total_pages": 2,
+    "data": [
+        {
+            "id": 7,
+            "email": "michael.lawson@reqres.in",
+            "first_name": "Michael",
+            "last_name": "Lawson",
+            "avatar": "https://reqres.in/img/faces/7-image.jpg"
+        },
+        {
+            "id": 8,
+            "email": "lindsay.ferguson@reqres.in",
+            "first_name": "Lindsay",
+            "last_name": "Ferguson",
+            "avatar": "https://reqres.in/img/faces/8-image.jpg"
+        },
+        {
+            "id": 9,
+            "email": "tobias.funke@reqres.in",
+            "first_name": "Tobias",
+            "last_name": "Funke",
+            "avatar": "https://reqres.in/img/faces/9-image.jpg"
+        }
+    ]
+}
+
+
+reqres_json = json.dumps(reqres_data, indent=2,separators=(",", " -> "))
+
+print(reqres_json)
