82dc920f4b79467a3616152af6d0b98cef7d7a49
Fernando Caballero <fernando.caballero@globant.com>
2022-08-02 19:32:53 -0300

    Json Exercise 5

---
+++ Python Json Exercises/Exercise-5.py
@@ -0,0 +1,55 @@
+import json
+
+sampleJson = """{ 
+   "company":{ 
+      "employee":{ 
+         "name":"emma",
+         "payble":{ 
+            "salary":7000,
+            "bonus":800
+         }
+      }
+   }
+}"""
+
+
+json_data = json.loads(sampleJson)
+print(json_data['company']['employee']['payble']['salary'])
+
+
+# sample data from api
+
+sample_json = """
+{
+    "page": 2,
+    "per_page": 6,
+    "total": 12,
+    "total_pages": 2,
+    "data": [
+        {
+            "id": 7,
+            "email": "michael.lawson@reqres.in",
+            "first_name": "Michael",
+            "last_name": "Lawson",
+            "avatar": "https://reqres.in/img/faces/7-image.jpg"
+        },
+        {
+            "id": 8,
+            "email": "lindsay.ferguson@reqres.in",
+            "first_name": "Lindsay",
+            "last_name": "Ferguson",
+            "avatar": "https://reqres.in/img/faces/8-image.jpg"
+        },
+        {
+            "id": 9,
+            "email": "tobias.funke@reqres.in",
+            "first_name": "Tobias",
+            "last_name": "Funke",
+            "avatar": "https://reqres.in/img/faces/9-image.jpg"
+        }
+    ]
+}"""
+
+json_format = json.loads(sample_json)
+
+print(json_format['data'][2]['email'])
