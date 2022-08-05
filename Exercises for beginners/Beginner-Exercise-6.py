48a5594142cdf14ac1d6a283826a0709dcf20b45
Fernando Caballero <fernando.caballero@globant.com>
2022-08-05 10:59:07 -0300

    Beginner Exercise 6

--- Exercises for beginners/Beginner-Exercise-6.py
+++ Exercises for beginners/Beginner-Exercise-6.py
@@ -5,6 +5,11 @@ Iterate the given list of numbers and print only those numbers which are divisib
 
 l1 = [1, 2, 5, 10, 4, 20, 55, 50, 3, 100]
 
+l2 = [1, 2, 3, 4, 5]
+
 div_5 = list(filter(lambda x: x % 5 == 0, l1))
 
+only_5 = list(filter(lambda x: x % 5 == 0, l2))
+
+print(only_5)
 print(div_5)
