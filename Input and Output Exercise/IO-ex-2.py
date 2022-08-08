7f369e1bebf1befea277548deeb9f24f7fbb3b87
Fernando Caballero <fernando.caballero@globant.com>
2022-08-08 17:47:00 -0300

    IO exercise 2

---
+++ Input and Output Exercise/IO-ex-2.py
@@ -0,0 +1,10 @@
+"""
+Use the print() function to format the given words in the mentioned format. Display the ** separator between each string
+
+Expected Output:
+
+For example: print('Name', 'Is', 'James') will display Name**Is**James
+"""
+
+print('My', 'name', 'is', 'Fernando', sep="**")
+
