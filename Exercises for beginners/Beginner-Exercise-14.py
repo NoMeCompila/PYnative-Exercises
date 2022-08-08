5a0516c3b0c79b638be2d2eabf3e56924e0299d0
Fernando Caballero <fernando.caballero@globant.com>
2022-08-08 17:22:05 -0300

    Beginner Exercise finished

---
+++ Exercises for beginners/Beginner-Exercise-14.py
@@ -0,0 +1,14 @@
+"""
+Print downward Half-Pyramid Pattern with Star (asterisk)
+* * * * *
+* * * *
+* * *
+* *
+*
+"""
+i = 5
+
+while i > 0:
+    print("* "*i)
+    i -= 1
+
---
+++ Exercises for beginners/Beginner-Exercise-15.py
@@ -0,0 +1,11 @@
+"""
+Exercise 15: Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+"""
+
+
+def exponencial(base, exponente):
+    return base ** exponente
+
+
+print(exponencial(2, 5))
+
